05bb3d1439e7bbe27ce7060214f97ea19df7a979
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-03-26 14:48:22 +0530

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express=require("express")
+const{connection} = require("./db")
+const { auth } = require("./middleware/authMiddleware")
+const { todoRouter } = require("./routes/todo.routes")
+const { userRouter } = require("./routes/user.routes")
+require("dotenv").config() 
+const cors=require("cors")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use("/user",userRouter)
+app.use(auth)
+app.use("/todo",todoRouter)
+
+
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("server is connected to db");
+    }catch(err){
+        console.log(err);
+    }
+    console.log("server is running");
+})
